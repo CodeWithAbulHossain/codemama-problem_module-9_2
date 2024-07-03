2dc0065e973352d5a2104524085290d27efe7e7d
abulhossainofficial <abulhossain.comc>
2024-07-03 20:27:29 +0600

    done

---
+++ index.js
@@ -0,0 +1,99 @@
+// problem 1
+function main(input) {
+  /**
+   * Write JavaScript code from here
+   */
+  // console.log(typeof input);
+  const year = input;
+  const a = parseInt(year);
+  // console.log(typeof a);
+  if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0) {
+    console.log(year + " is a leap year.");
+  } else {
+    console.log(year + " is not a leap year.");
+  }
+}
+
+let input = "";
+process.stdin.on("data", (chunk) => {
+  input += chunk;
+});
+
+process.stdin.on("end", () => {
+  main(input.trim());
+});
+
+// problem 2
+function main(input) {
+  /**
+   * Write JavaScript code from here
+   */
+  // console.log(typeof input);
+  const year = input;
+  const a = parseInt(year);
+  // console.log(typeof a);
+  if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0) {
+    console.log(year + " is a leap year.");
+  } else {
+    console.log(year + " is not a leap year.");
+  }
+}
+
+let input = "";
+process.stdin.on("data", (chunk) => {
+  input += chunk;
+});
+
+process.stdin.on("end", () => {
+  main(input.trim());
+});
+
+// problem 3
+function main(input) {
+  /**
+   * Write JavaScript code from here
+   */
+  // console.log(typeof input);
+  const year = input;
+  const a = parseInt(year);
+  // console.log(typeof a);
+  if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0) {
+    console.log(year + " is a leap year.");
+  } else {
+    console.log(year + " is not a leap year.");
+  }
+}
+
+let input = "";
+process.stdin.on("data", (chunk) => {
+  input += chunk;
+});
+
+process.stdin.on("end", () => {
+  main(input.trim());
+});
+
+// problem 4
+function main(input) {
+  /**
+   * Write JavaScript code from here
+   */
+  // console.log(typeof input);
+  const year = input;
+  const a = parseInt(year);
+  // console.log(typeof a);
+  if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0) {
+    console.log(year + " is a leap year.");
+  } else {
+    console.log(year + " is not a leap year.");
+  }
+}
+
+let input = "";
+process.stdin.on("data", (chunk) => {
+  input += chunk;
+});
+
+process.stdin.on("end", () => {
+  main(input.trim());
+});
